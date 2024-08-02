be17fa9d7bd1fd755282c98c970a6bd86ef001b3
Harry Dhillon <hdhillon478@gmail.com>
2024-08-02 15:25:19 +0100

    chore: Add lib folder with constants.ts file

---
+++ src/lib/constants.ts
@@ -0,0 +1,17 @@
+// Definition of constants used in the application
+
+// nav links used in the desktop menu and mobile menu navbar components
+export const navLinks = [
+  {
+    href: "/",
+    text: "Home",
+  },
+  {
+    href: "/properties",
+    text: "Properties",
+  },
+  {
+    href: "/properties/add",
+    text: "Add Property",
+  },
+];
