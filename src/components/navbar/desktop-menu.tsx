f096f95d77fa5af6675c43696a369f4cd3e2947c
Harry Dhillon <hdhillon478@gmail.com>
2024-08-02 15:55:02 +0100

    refactor: Update navbar menu components and links

--- src/components/navbar/desktop-menu.tsx
+++ src/components/navbar/desktop-menu.tsx
@@ -1,28 +1,37 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { navLinks } from "@/lib/constants";
+import { MenuItem } from "./types";
+
+const menuItemStyles =
+  "text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium";
 
 export default function DesktopMenu() {
+  const pathname = usePathname();
+
   return (
     <div className='hidden md:ml-6 md:block'>
       <div className='flex space-x-2'>
-        <Link
-          href='/'
-          className='text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-        >
-          Home
-        </Link>
-        <Link
-          href='/properties'
-          className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-        >
-          Properties
-        </Link>
-        <Link
-          href='/properties/add'
-          className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-        >
-          Add Property
-        </Link>
+        {navLinks.map((link, index) => (
+          <DesktopMenuItem key={index} link={link} pathname={pathname} />
+        ))}
       </div>
     </div>
+  );
+}
+
+function DesktopMenuItem({ link, pathname }: MenuItem) {
+  return (
+    <Link
+      href={link.href}
+      className={`${
+        pathname === link.href ? "bg-gray-700" : ""
+      } ${menuItemStyles}`}
+    >
+      {link.text}
+    </Link>
   );
 }
--- src/components/navbar/mobile-menu.tsx
+++ src/components/navbar/mobile-menu.tsx
@@ -1,32 +1,41 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { navLinks } from "@/lib/constants";
+import { MenuItem } from "./types";
+
+const menuItemStyles =
+  "text-white block rounded-md px-3 py-2 text-base font-medium";
 
 export default function MobileMenu() {
+  const pathname = usePathname();
+
   return (
     <div id='mobile-menu'>
-      <div className='space-y-1 px-2 pb-3 pt-2'>
-        <Link
-          href='/'
-          className='bg-black text-white block rounded-md px-3 py-2 text-base font-medium'
-        >
-          Home
-        </Link>
-        <Link
-          href='/properties'
-          className='text-white block rounded-md px-3 py-2 text-base font-medium'
-        >
-          Properties
-        </Link>
-        <Link
-          href='/properties/add'
-          className='text-white block rounded-md px-3 py-2 text-base font-medium'
-        >
-          Add Property
-        </Link>
+      <div className=' md:hidden space-y-1 px-2 pb-3 pt-2'>
+        {navLinks.map((link, index) => (
+          <MobileMenuItem key={index} link={link} pathname={pathname} />
+        ))}
         <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5'>
           <i className='fa-brands fa-google mr-2'></i>
           <span>Login or Register</span>
         </button>
       </div>
     </div>
+  );
+}
+
+function MobileMenuItem({ link, pathname }: MenuItem) {
+  return (
+    <Link
+      href={link.href}
+      className={` ${
+        pathname === link.href ? "bg-gray-700" : ""
+      } ${menuItemStyles}`}
+    >
+      {link.text}
+    </Link>
   );
 }
