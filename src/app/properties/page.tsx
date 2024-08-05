70fe652819e7877a3ff12b9cc255297fa2e5fc72
Harry Dhillon <hdhillon478@gmail.com>
2024-08-05 08:40:04 +0100

    feat: Add Properties page component

---
+++ src/app/properties/page.tsx
@@ -0,0 +1,19 @@
+import PropertyCard from "@/components/properties/property-card";
+import properties from "./data/properties.json";
+export default function Properties() {
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length === 0 ? (
+          <p>No properties found...</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
