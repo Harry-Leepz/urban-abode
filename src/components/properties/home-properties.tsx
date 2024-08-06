c14ced85e6205834b441165a77848c41b3ae0d12
Harry Dhillon <hdhillon478@gmail.com>
2024-08-06 14:52:52 +0100

    feat: Add HomeProperties component to display recent properties on homepage

---
+++ src/components/properties/home-properties.tsx
@@ -0,0 +1,37 @@
+import properties from "@/data/properties.json";
+import PropertyCard from "./property-card";
+import Link from "next/link";
+
+export default function HomeProperties() {
+  const recentProperties = properties.slice(0, 3);
+
+  return (
+    <>
+      <section className='px-4 py-6 bg-slate-100'>
+        <div className='container-xl lg:container m-auto px-4 py-6 '>
+          <h2 className='text-3xl font-bold text-slate-900 mb-6 text-center'>
+            Recent Properties
+          </h2>
+          {properties.length === 0 ? (
+            <p>No properties found...</p>
+          ) : (
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+              {recentProperties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+
+      <section className='m-auto max-w-lg my-6 px-6'>
+        <Link
+          href='/properties'
+          className='block text-center py-4 text-white rounded-xl px-6 bg-slate-900 hover:bg-slate-600'
+        >
+          View All Properties
+        </Link>
+      </section>
+    </>
+  );
+}
