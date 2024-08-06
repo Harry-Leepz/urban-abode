667a078a6d18e2cbc4ecb18ef03cbc075a6148fb
Harry Dhillon <hdhillon478@gmail.com>
2024-08-06 14:53:32 +0100

    feat: Add HomeProperties component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/hero";
 import InfoBoxes from "@/components/home/info-boxes";
+import HomeProperties from "@/components/properties/home-properties";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 }
