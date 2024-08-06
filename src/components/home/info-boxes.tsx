2f3b9414eefb3f056d8e564d38daf0969b810c8f
Harry Dhillon <hdhillon478@gmail.com>
2024-08-06 14:53:53 +0100

    feat: Add margin-bottom to InfoBoxes component

--- src/components/home/info-boxes.tsx
+++ src/components/home/info-boxes.tsx
@@ -25,7 +25,7 @@ export default function InfoBoxes() {
   return (
     <section>
       <div className='container-xl lg:container m-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg mb-8'>
           {infoBoxeData.map((infoBox, index) => (
             <InfoBox key={index} {...infoBox} />
           ))}
