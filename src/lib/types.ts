0404fe54e21f94c4907bea6ec77d4ec4cc7a0eb4
Harry Dhillon <hdhillon478@gmail.com>
2024-08-08 10:25:23 +0100

    refactor: Update Property interface to use TypeScript type aliases

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,37 +1,37 @@
-export interface Property {
+export type TProperty = {
   _id: string;
   owner: string;
   name: string;
   type: string;
   description: string;
-  location: Location;
+  location: TLocation;
   beds: number;
   baths: number;
   square_feet: number;
   amenities: string[];
-  rates: Rates;
-  seller_info: SellerInfo;
+  rates: TRates;
+  seller_info: TSellerInfo;
   images: string[];
   is_featured: boolean;
   createdAt: string;
   updatedAt: string;
-}
+};
 
-export interface Location {
+export type TLocation = {
   street: string;
   city: string;
   state: string;
   zipcode: string;
-}
+};
 
-export interface Rates {
+export type TRates = {
   weekly?: number;
   monthly?: number;
   nightly?: number;
-}
+};
 
-export interface SellerInfo {
+export type TSellerInfo = {
   name: string;
   email: string;
   phone: string;
-}
+};
