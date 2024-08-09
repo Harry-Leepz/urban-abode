d7a666a5e04225d5abb371fc342ca19ff6739fa5
Harry Dhillon <hdhillon478@gmail.com>
2024-08-09 15:39:15 +0100

    feat: Update PropertyDetails component to display property header image

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
+import { FaArrowLeft } from "react-icons/fa";
+
 import PropertyHeaderImage from "@/components/properties/property-header-image";
+import PropertyInfo from "@/components/properties/property-info";
+
+import { TProperty } from "@/lib/types";
 
 import connectDb from "../../../../config/database";
 import Property from "../../../../models/Property";
-import { TProperty } from "@/lib/types";
-import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa";
 
 type PropertyDetailsProps = {
   params: {
@@ -19,11 +23,15 @@ export default async function PropertyDetails({
 
   const property: TProperty | null = await Property.findById(params.id).lean();
 
+  if (!property) {
+    return <div>Property not found</div>;
+  }
+
   return (
     <>
       <PropertyHeaderImage
-        propertyImage={property?.images[0] || ""}
-        propertyName={property?.name || ""}
+        propertyImage={property.images[0]}
+        propertyName={property.name}
       />
       <section>
         <div className='container m-auto py-6 px-6'>
@@ -40,6 +48,7 @@ export default async function PropertyDetails({
         <div className='container m-auto py-10 px-6'>
           <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
             {/*  Property Details */}
+            <PropertyInfo />
           </div>
         </div>
       </section>
