5a4aefa646b2879196cfcdadad121884ac229349
Harry Dhillon <hdhillon478@gmail.com>
2024-08-09 15:29:38 +0100

    feat: Add back button to PropertyDetails page

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -3,6 +3,8 @@ import PropertyHeaderImage from "@/components/properties/property-header-image";
 import connectDb from "../../../../config/database";
 import Property from "../../../../models/Property";
 import { TProperty } from "@/lib/types";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
 type PropertyDetailsProps = {
   params: {
@@ -23,7 +25,24 @@ export default async function PropertyDetails({
         propertyImage={property?.images[0] || ""}
         propertyName={property?.name || ""}
       />
-      <section></section>
+      <section>
+        <div className='container m-auto py-6 px-6'>
+          <Link
+            href='/properties'
+            className='text-slate-900 flex items-center hover:underline'
+          >
+            <FaArrowLeft className='mr-2 cursor-pointer' />
+            Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className='bg-slate-100'>
+        <div className='container m-auto py-10 px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            {/*  Property Details */}
+          </div>
+        </div>
+      </section>
     </>
   );
 }
