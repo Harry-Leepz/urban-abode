14a5e8ce5aee6447d945f20fd78525ef11336e3a
Harry Dhillon <hdhillon478@gmail.com>
2024-08-09 15:16:40 +0100

    refactor: Update PropertyDetails component to fetch and display property header image

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -1,3 +1,29 @@
-export default function PropertyDetails() {
-  return <div>Property Details</div>;
+import PropertyHeaderImage from "@/components/properties/property-header-image";
+
+import connectDb from "../../../../config/database";
+import Property from "../../../../models/Property";
+import { TProperty } from "@/lib/types";
+
+type PropertyDetailsProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function PropertyDetails({
+  params,
+}: PropertyDetailsProps) {
+  connectDb();
+
+  const property: TProperty | null = await Property.findById(params.id).lean();
+
+  return (
+    <>
+      <PropertyHeaderImage
+        propertyImage={property?.images[0] || ""}
+        propertyName={property?.name || ""}
+      />
+      <section></section>
+    </>
+  );
 }
