b665c5b881176b89c29ae116a68949f1ab6fb1ab
Harry Dhillon <hdhillon478@gmail.com>
2024-08-13 15:55:34 +0100

    chore: Add styling to the add property page

--- src/app/properties/add/page.tsx
+++ src/app/properties/add/page.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import AddPropertyForm from "@/components/properties/add-property-form";
 
 export default function AddProperties() {
-  return <div>Add new properties</div>;
+  return (
+    <section className='bg-slate-100'>
+      <div className='container m-auto max-w-2xl py-24'>
+        <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
+          <AddPropertyForm />
+        </div>
+      </div>
+    </section>
+  );
 }
